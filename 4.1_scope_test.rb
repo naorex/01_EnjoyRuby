81cff3950e4b490413651bb876a47e1417118270
naorex <nao23r@gmail.com>
2025-04-14 21:36:37 +0900

    today's progress

---
+++ 4.1_scope_test.rb
@@ -0,0 +1,10 @@
+
+# scope test of variable
+
+$x = 0
+x = 0
+
+require_relative "4.1_sub"
+
+p $x
+p x
---
+++ 4.2_input_variable_behavior.rb
@@ -0,0 +1,6 @@
+
+# variable input behavior
+
+first, second, *rest = 1,2,3,4,5
+
+p *rest # => [3,4,5]
